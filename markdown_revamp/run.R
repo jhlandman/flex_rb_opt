4b7b4a97cc81b286a8c9cca09e93546f7f718ba2
Jay-Land-Man <jaysonhenrylandman@yahoo.com>
2020-07-05 01:05:40 +0200

    finishing until ch 4

--- markdown_revamp/run.R
+++ markdown_revamp/run.R
@@ -6,4 +6,6 @@ bookdown::serve_book() # ctrl + shift + F10 to reset
 # for pdf compilations (only every once in a while)
 # retains .tex artifact
 # make sure to use ctrl + shift + F10 before use
-bookdown::render_book()
+bookdown::render_book("index.Rmd")
+
+# to publish to github pages just copy the book_folder over the ../docs folder with the same name
